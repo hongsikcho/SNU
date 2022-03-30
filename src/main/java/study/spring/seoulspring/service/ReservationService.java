00fd9e44371a72a2852230277863151644e2294b
DESKTOP-C19OHNO\호수 <hs020600@naver.com>
2022-03-30 22:00:24 +0900

    1234

---
+++ src/main/java/study/spring/seoulspring/service/ReservationService.java
@@ -0,0 +1,8 @@
+package study.spring.seoulspring.service;
+import study.spring.seoulspring.model.Reservation;
+
+public interface ReservationService {
+	public abstract int insertTime(Reservation input) throws Exception;
+	
+
+}
